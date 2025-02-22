f094b417674a85ec4811b32f90a43fc9d37e194f
GadigeShilpa <153886246+GadigeShilpa@users.noreply.github.com>
2025-02-23 00:20:07 +0530

    Add files via upload

---
+++ JUnit/Activities/NotEnoughFundsException.java
@@ -0,0 +1,9 @@
+package examples;
+
+public class NotEnoughFundsException extends RuntimeException{
+	
+	 public NotEnoughFundsException(int amount, int balance) {
+	        super("Attempted to withdraw " + amount + " with a balance of " + balance);
+	    }
+
+}
